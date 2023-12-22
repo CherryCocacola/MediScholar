fedca869b1929e9c98580243ac6875074260992d
h2yjang <h3yjang@naver.com>
2023-12-22 20:14:52 +0900

    <!--2023.12.22--!> 장하연

--- Medi_scholar/src/main/java/com/medi/pubmed/member/MediMemberDAO.java
+++ Medi_scholar/src/main/java/com/medi/pubmed/member/MediMemberDAO.java
@@ -13,44 +13,53 @@ public class MediMemberDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
-		//사용자 정보
-		public HashMap<String, Object> getUserInfo(HashMap<String, Object> param) {
-			return sqlSession.selectOne("getUserInfo", param);
-		}
+	// 디비에 구글이메일 유무확인
+	public int checkEmailExist(String email) {
+		return sqlSession.selectOne("checkEmailExist", email);
+
+	}
+
+	// 구글로그인 계정 정보
+	public HashMap<String, Object> googleUserInfo(HashMap<String, Object> param) {
+		return sqlSession.selectOne("googleUserInfo", param);
+	}
+
+	// 사용자 로그인 정보
+	public HashMap<String, Object> getUserInfo(HashMap<String, Object> param) {
+		return sqlSession.selectOne("getUserInfo", param);
+	}
+	
+	// 회원가입 아이디 중복 체크
+	public int isUserIdDuplicate(String userId) {
+		return sqlSession.selectOne("isUserIdDuplicate", userId);
+	}
+
+	// 회원가입
+	public int insertUserInfo(HashMap<String, Object> param) {
+		return sqlSession.insert("insertUserInfo", param);
+
+	}
+
+	// 국가 선택지 리스트
+	public List<HashMap<String, Object>> getnation(HashMap<String, Object> param) {
+		return sqlSession.selectList("getnation", param);
+	}
+
+	// 직업 선택지 리스트
+	public List<HashMap<String, Object>> getjob(HashMap<String, Object> param) {
+		return sqlSession.selectList("getjob", param);
+	}
+
+	// 주분야 선택지 리스트
+	public List<HashMap<String, Object>> getsignList(String primary) {
+		return sqlSession.selectList("getsignList", primary);
+	}
+
+	// 관심분야 주
+	public List<HashMap<String, Object>> getpriList(HashMap<String, Object> param) {
+		return sqlSession.selectList("getpriList", param);
+	}
 		
-		//관심분야 주
-		public List<HashMap<String, Object>> getpriList(HashMap<String, Object> param) {
-			return sqlSession.selectList("getpriList", param);
-		}
-		//회원가입
-		public int insertUserInfo(HashMap<String, Object> param) {
-			return sqlSession.insert("insertUserInfo",param);
-			
-		}
-		//디비에 이메일 유무확인
-		public int checkEmailExistsForToken(String email) {
-			return sqlSession.selectOne("checkEmailExistsForToken",email);
-			
-		}//구글 계정 정보
-		public HashMap<String, Object> googleUserInfo(HashMap<String, Object> param) {
-			return sqlSession.selectOne("googleUserInfo",param);
-		}
-		//국가 선택지 리스트
-		public List<HashMap<String, Object>> getnation(HashMap<String, Object> param) {
-			return sqlSession.selectList("getnation",param);
-		}
-		//직업 선택지 리스트
-		public List<HashMap<String, Object>> getjob(HashMap<String, Object> param) {
-			return sqlSession.selectList("getjob",param);
-		}
-		//주분야 선택지 리스트
-		public List<HashMap<String, Object>> getsignList(String primary) {
-			return sqlSession.selectList("getsignList",primary);
-		}
-		//세부분야 선택지 리스트
-		public int isUserIdDuplicate(String userId) {
-			return sqlSession.selectOne("isUserIdDuplicate",userId);
-		}
 
 
 	}
