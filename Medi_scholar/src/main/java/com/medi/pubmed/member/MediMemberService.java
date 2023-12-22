65e505af7243cdda779eee292a523df1d88683e3
RIWON <riwonkim95@naver.com>
2023-12-22 17:39:36 +0900

    /23-12-22/ 작성자 : 김리원 수정 내용 : 선택 박스 목록 보이기

--- Medi_scholar/src/main/java/com/medi/pubmed/member/MediMemberService.java
+++ Medi_scholar/src/main/java/com/medi/pubmed/member/MediMemberService.java
@@ -6,20 +6,32 @@ import java.util.List;
 public interface MediMemberService {
 	//사용자 정보 조회
 	HashMap<String, Object> getUserInfo(HashMap<String, Object> param); 
+	
 	//사용자 정보 입력
 	void insertUserInfo(HashMap<String, Object> param) ;
+	
 	//구글을 통한 로그인 시 회원 가입 여부
 	int checkEmailExistsForToken(String email);
+	
 	//구글을 통한 로그인 시 회원 가입한 사용자의 사용자 정보 조회
 	HashMap<String, Object> googleUserInfo(HashMap<String, Object> param);
+	
+	// 중복아이디 조회
+	int isUserIdDuplicate(String userId);
+	
+	//국가 선택지 목록
+	List<HashMap<String, Object>> getnation(HashMap<String, Object> param);
+	
+	//직업 선택지 목록
+	List<HashMap<String, Object>> getjob(HashMap<String, Object> param);
+	
 	//관심 주분야 목록 조회
 	List<HashMap<String, Object>> getpriList(HashMap<String, Object> param);
-	//나라 목록 조회
-	List<HashMap<String, Object>> getnation(HashMap<String, Object> param);
-	//직업 목록 조회
-	List<HashMap<String, Object>> getjob(HashMap<String, Object> param);
-	//세부 분야 조회
+	
+	//
 	List<HashMap<String, Object>> getsignList(String primary);
-	//db 아이디유무
-	int isUserIdDuplicate(String userId);
+	
+
+	
+	
 }
